defe32865eb4f972dc183d0c30d9407b6eed6a8e
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-09 13:48:05 +0100

    creation et implementation des services membre

---
+++ src/main/java/com/example/aftas/domain/Member.java
@@ -0,0 +1,33 @@
+package com.example.aftas.domain;
+
+import com.example.aftas.enums.IdentityDocumentType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String familyName;
+    private Date accessionDate;
+    private String nationality;
+    private IdentityDocumentType identityDocumentType;
+    private String identityNumber;
+
+
+
+}
---
+++ src/main/java/com/example/aftas/service/MemberService.java
@@ -0,0 +1,6 @@
+package com.example.aftas.service;
+
+public interface MemberService {
+}
+
+
