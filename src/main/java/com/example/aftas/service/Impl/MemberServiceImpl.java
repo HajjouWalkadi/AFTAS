be3f14fdf70b2d9fb42882ed953289d42da2330a
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-09 16:18:14 +0100

    ajouter member service et son implementation

--- src/main/java/com/example/aftas/service/Impl/MemberServiceImpl.java
+++ src/main/java/com/example/aftas/service/Impl/MemberServiceImpl.java
@@ -1,4 +1,41 @@
 package com.example.aftas.service.Impl;
 
-public class MemberServiceImpl {
+import com.example.aftas.domain.Member;
+import com.example.aftas.repository.MemberRepository;
+import com.example.aftas.service.MemberService;
+
+import java.util.List;
+
+public class MemberServiceImpl implements MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    @Override
+    public Member getMemberById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Member addMember(Member member) {
+        return null;
+    }
+
+    @Override
+    public List<Member> searchMember(String name) {
+        return null;
+    }
+
+    @Override
+    public Member updateMember(Member member, Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteMember(Long id) {
+
+    }
 }
--- src/main/java/com/example/aftas/service/MemberService.java
+++ src/main/java/com/example/aftas/service/MemberService.java
@@ -1,6 +1,15 @@
 package com.example.aftas.service;
 
+import com.example.aftas.domain.Member;
+
+import java.util.List;
+
 public interface MemberService {
+    Member getMemberById(Long id);
+    Member addMember(Member member);
+    List<Member> searchMember(String name);
+    Member updateMember(Member member, Long id);
+    void deleteMember(Long id);
 }
 
 
