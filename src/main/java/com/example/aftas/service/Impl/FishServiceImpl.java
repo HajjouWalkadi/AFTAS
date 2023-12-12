def4d3f44ba62c4582c7f96f5b32e84c23172b0d
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-12 19:26:44 +0100

    fish service is well added

--- src/main/java/com/example/aftas/service/Impl/FishServiceImpl.java
+++ src/main/java/com/example/aftas/service/Impl/FishServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.aftas.service.Impl;
 
 import com.example.aftas.domain.Fish;
+import com.example.aftas.domain.Level;
+import com.example.aftas.handler.exception.ResourceNotFoundException;
 import com.example.aftas.repository.FishRepository;
 import com.example.aftas.service.FishService;
 import org.springframework.stereotype.Service;
@@ -11,29 +13,51 @@ import java.util.List;
 public class FishServiceImpl implements FishService {
 
     private FishRepository fishRepository;
+    private LevelServiceImpl levelService;
 
+
+    public FishServiceImpl(FishRepository fishRepository, LevelServiceImpl levelService) {
+        this.fishRepository = fishRepository;
+        this.levelService = levelService;
+    }
     @Override
     public Fish getFishById(Long id) {
-        return null;
+        return fishRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Fish id " + id + "not found"));
     }
 
     @Override
     public List<Fish> getAllFishes() {
-        return null;
+        return fishRepository.findAll();
     }
 
     @Override
     public Fish addFish(Fish fish) {
-        return null;
+        // check if fish name is not already exist
+        if (fishRepository.findByName(fish.getName()) != null) {
+            throw new ResourceNotFoundException("Fish name " + fish.getName() + " already exists");
+        }
+        // check if level is already exist
+        if(levelService.getLevelById(fish.getLevel().getId()) == null) {
+            throw new ResourceNotFoundException("Level id " + fish.getLevel().getId() + "not found");
+        }
+        return fishRepository.save(fish);
     }
 
     @Override
     public Fish updateFish(Fish fish, Long id) {
-        return null;
+        Fish fish1 = getFishById(id);
+        fish1.setName(fish.getName());
+        fish1.setAverageWeight(fish.getAverageWeight());
+
+        if (levelService.getLevelById(fish.getLevel().getId()) == null) {
+            throw new ResourceNotFoundException("Level id " + fish.getLevel().getId() + "not found");
+        }
+        fish1.setLevel(fish.getLevel());
+        return fishRepository.save(fish1);
     }
 
     @Override
     public void deleteFish(Long id) {
-
+        fishRepository.deleteById(id);
     }
 }
