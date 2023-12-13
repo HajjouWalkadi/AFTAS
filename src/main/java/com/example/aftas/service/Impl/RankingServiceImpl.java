44ead14f1694445a199c633ea2ea5db837c7b733
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-13 11:46:15 +0100

    ranking service implementation is added

--- src/main/java/com/example/aftas/service/Impl/RankingServiceImpl.java
+++ src/main/java/com/example/aftas/service/Impl/RankingServiceImpl.java
@@ -25,27 +25,42 @@ public class RankingServiceImpl implements RankingService {
 
     @Override
     public Ranking getRankingByCompetitionIdAndMemberId(Long competitionId, Long memberId) {
-        
-        return null;
+        //check if competition exists
+        competitionService.getCompetitionById(competitionId);
+        Ranking ranking = rankingRepository.findByMemberIdAndCompetitionId(memberId,competitionId);
+        if (ranking == null) {
+            throw new RuntimeException("Member id " + memberId + "not found");
+        }
+        //check if member exists
+        memberService.getMemberById(memberId);
+        return ranking;
+
     }
 
     @Override
     public Ranking addRanking(Ranking ranking) {
-        return null;
+        return rankingRepository.save(ranking);
     }
 
     @Override
     public Ranking updateRanking(Ranking ranking, Long id) {
-        return null;
+        Ranking ranking1 = getRankingById(id);
+        ranking1.setRank(ranking.getRank());
+        ranking1.setScore(ranking.getScore());
+        return rankingRepository.save(ranking1);
     }
 
     @Override
     public Ranking updateRankingScore(Ranking ranking, Long id) {
-        return null;
+        Ranking ranking1 = getRankingById(id);
+        ranking1.setScore(ranking.getScore()+ranking1.getScore());
+        return rankingRepository.save(ranking1);
     }
 
     @Override
     public void deleteRanking(Long id) {
+        getRankingById(id);
+        rankingRepository.deleteById(id);
 
     }
 }
