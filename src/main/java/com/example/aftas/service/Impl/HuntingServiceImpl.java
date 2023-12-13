946a329c82e81e03e3cdcc543a006f5f56b5a81c
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-14 00:20:11 +0100

    Hunting service implementation is added

--- src/main/java/com/example/aftas/service/Impl/HuntingServiceImpl.java
+++ src/main/java/com/example/aftas/service/Impl/HuntingServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.aftas.service.Impl;
 
 import com.example.aftas.domain.Fish;
 import com.example.aftas.domain.Hunting;
+import com.example.aftas.domain.Ranking;
 import com.example.aftas.repository.HuntingRepository;
 import com.example.aftas.service.HuntingService;
 import org.springframework.stereotype.Service;
@@ -14,35 +15,61 @@ public class HuntingServiceImpl implements HuntingService {
     private HuntingRepository huntingRepository;
     private MemberServiceImpl memberService;
     private CompetitionServiceImpl competitionService;
+    private FishServiceImpl fishService;
+    private RankingServiceImpl rankingService;
 
-
-    public HuntingServiceImpl(HuntingRepository huntingRepository) {
+    public HuntingServiceImpl(HuntingRepository huntingRepository, MemberServiceImpl memberService, CompetitionServiceImpl competitionService, FishServiceImpl fishService, RankingServiceImpl rankingService) {
         this.huntingRepository = huntingRepository;
+        this.memberService = memberService;
+        this.competitionService = competitionService;
+        this.fishService = fishService;
+        this.rankingService = rankingService;
     }
+
 
     @Override
     public Hunting getHuntingById(Long id) {
-        return huntingRepository.findById(id).orElse(null);
+        return huntingRepository.findById(id).orElseThrow(() -> new RuntimeException("Hunting id " + id + "not found"));
     }
 
     @Override
     public Hunting addHunting(Hunting hunting) {
-        return huntingRepository.save(hunting);
+        Long memberId = hunting.getMember().getId();
+        Long competitionId = hunting.getCompetition().getId();
+        Long fishId = hunting.getFish().getId();
+
+        memberService.getMemberById(memberId);
+        competitionService.getCompetitionById(competitionId);
+        fishService.getFishById(fishId);
+        rankingService.getRankingByCompetitionIdAndMemberId(competitionId, memberId);
+        Hunting hunting1 = huntingRepository.findByCompetitionIdAndMemberIdAndFishId(competitionId, memberId, fishId);
+
+        Ranking ranking = rankingService.getRankingByCompetitionIdAndMemberId(competitionId, memberId);
+        ranking.setScore(ranking.getScore() + hunting.getFish().getLevel().getPoints());
+        rankingService.updateRanking(ranking, ranking.getId());
+
+        if (hunting1 != null) {
+            hunting1.setNumberOfFish(hunting1.getNumberOfFish() + 1);
+            return huntingRepository.save(hunting1);
+        }else {
+            return huntingRepository.save(hunting);
+        }
     }
 
     @Override
     public List<Hunting> getHuntingsByCompetition(Long competitionId) {
-        return null;
+        return huntingRepository.findByCompetitionId(competitionId);
     }
 
     @Override
     public List<Hunting> getHuntingsByCompetitionAndMember(Long competitionId, Long memberId) {
-        return null;
+        return huntingRepository.findByCompetitionIdAndMemberId(competitionId, memberId);
     }
 
 
     @Override
     public void deleteHunting(Long id) {
+        getHuntingById(id);
         huntingRepository.deleteById(id);
     }
 }
