e0a2b23b509cdaf7f2816640cbbe296e6c5ad304
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-14 11:21:54 +0100

    competition service is modified

--- src/main/java/com/example/aftas/service/CompetitionService.java
+++ src/main/java/com/example/aftas/service/CompetitionService.java
@@ -3,19 +3,20 @@ package com.example.aftas.service;
 import com.example.aftas.domain.Competition;
 import com.example.aftas.domain.Member;
 import com.example.aftas.domain.Ranking;
+import com.example.aftas.domain.RankingId;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public interface CompetitionService {
 
-    Competition getCompetitionById(Long id);
+    Competition getCompetitionById(Long code);
     Competition addCompetition(Competition competition);
     List<Competition> getAllCompetitions();
     List<Competition> searchCompetition(String code);
     Competition updateCompetition(Competition competition, Long id);
     void deleteCompetition(Long id);
-    Ranking rankingCompetitionResult(Ranking ranking, Long id);
+    Ranking rankingCompetitionResult(Ranking ranking, RankingId id);
     Ranking registerMemberCompetition(Ranking ranking);
 
 }
