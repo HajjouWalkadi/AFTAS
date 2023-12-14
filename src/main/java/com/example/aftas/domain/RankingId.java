dce245557cad7d3a49fa095c54b8ad7cab6a3f4c
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-14 11:34:36 +0100

    rankingId file is well added

--- src/main/java/com/example/aftas/domain/RankingId.java
+++ src/main/java/com/example/aftas/domain/RankingId.java
@@ -1,2 +1,23 @@
-package com.example.aftas.domain;public class RankingId {
+package com.example.aftas.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class RankingId implements Serializable {
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "competition_id")
+    private Long competitionId;
 }
