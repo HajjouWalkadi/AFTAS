7693eec5690f49fca5862b2ad5d6e9b97856ce07
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-15 22:42:18 +0100

    Hunting update dto is modified

--- src/main/java/com/example/aftas/dto/HuntingUpdateRequestDTO.java
+++ src/main/java/com/example/aftas/dto/HuntingUpdateRequestDTO.java
@@ -1,2 +1,17 @@
-package com.example.aftas.dto;public class HuntingUpdateRequestDTO {
+package com.example.aftas.dto;
+
+import com.example.aftas.domain.Hunting;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record HuntingUpdateRequestDTO(
+        @NotNull(message = "Number of fish is required")
+        @Min(value = 1, message = "Number of fish must be greater than 0")
+        int numberOfFish
+) {
+    public Hunting toHunting() {
+        return Hunting.builder()
+                .numberOfFish(numberOfFish)
+                .build();
+    }
 }
